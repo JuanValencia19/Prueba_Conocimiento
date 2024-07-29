63cfecc032f899300a220fabd7e038845e7406a5
JuanValencia19 <juan.valencia10@utp.edu.co>
2024-07-29 16:12:30 -0500

    Terminada la logica en base a una plantilla de internet con cambios realizados por mi en algunas configuraciones y documentaciones

--- logica.js
+++ logica.js
@@ -1,72 +1,71 @@
 "use strict";
-// Utilizando la api de openWheater
+//Basada en plantilla de internet del autor CodingManish
 const API = "1e3943c3f0a4c1c79361ad8f78e518af";
 
-const dayEl = document.querySelector("dia_predeterminado");
-const dateEl = document.querySelector("fecha_predeterminado");
-const btnEl = document.querySelector("btn_busqueda");
-const inputEl = document.querySelector("buscador");
+const dayEl = document.querySelector(".default_day");
+const dateEl = document.querySelector(".default_date");
+const btnEl = document.querySelector(".btn_search");
+const inputEl = document.querySelector(".input_field");
 
-const IconsContainer = document.querySelector(".icons");
+const iconsContainer = document.querySelector(".icons");
 const dayInfoEl = document.querySelector(".day_info");
 const listContentEl = document.querySelector(".list_content ul");
 
-const dias = [
-    "Domingo",
-    "Lunes",
-    "Martes",
-    "Miercoles",
-    "Jueves",
-    "Viernes",
-    "Sabado"
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
 ];
 
 // Mostrar el dia
 const day = new Date();
-const dayName = dias[day.getDay()];
+const dayName = days[day.getDay()];
 dayEl.textContent = dayName;
 
-//Mostrar la fecha
-let mes = day.toLocaleString("default", {month: "long"});
-let fecha = day.getDate();
-let ano = getFullYear();
+// Mostar la fecha
+let month = day.toLocaleString("default", { month: "long" });
+let date = day.getDate();
+let year = day.getFullYear();
 
 console.log();
-dateEl.textContent = fecha + "" + mes + "" + ano;
+dateEl.textContent = date + " " + month + " " + year;
 
-//Agregar evento
-btnEl.addEventListener("click", (e) =>{
-    e.preventDefault();
+// agregar evento
+btnEl.addEventListener("click", (e) => {
+  e.preventDefault();
 
-    // Revisar que el campo este vacio
-    if(inputEl.value !== ""){
-        const busqueda = inputEl.value;
-        inputEl.value = "";
-        findLocation(busqueda);
-    } else{
-        console.log("Por favor ingresa una ciudad o el nombre de un Pais");
-    }
+  // verificar si el boton fue enviado vacio
+  if (inputEl.value !== "") {
+    const Search = inputEl.value;
+    inputEl.value = "";
+    findLocation(Search);
+  } else {
+    console.log("Por favor ingrese una ciudad o un pais");
+  }
 });
 
-async function findLocation(nombre) {
-    IconsContainer.innerHTML = "";
-    dayInfoEl.innerHTML = "";
-    listContentEl.innerHTML = "";
-
-try {
+async function findLocation(name) {
+  iconsContainer.innerHTML = "";
+  dayInfoEl.innerHTML = "";
+  listContentEl.innerHTML = "";
+  try {
     const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${API}`;
     const data = await fetch(API_URL);
     const result = await data.json();
     console.log(result);
 
     if (result.cod !== "404") {
-      // display image content
+      // Mostrar imagen en caso de que no coloque una ciudad o pais correctamente
       const ImageContent = displayImageContent(result);
 
       // display right side content
       const rightSide = rightSideContent(result);
 
-      // forecast function
+      // funcion para los pronosticos
       displayForeCast(result.coord.lat, result.coord.lon);
 
       setTimeout(() => {
@@ -82,3 +81,68 @@ try {
   } catch (error) {}
 }
 
+// Mostrar la imagen y la temperatura
+function displayImageContent(data) {
+  return `
+    <h2 class="weather_temp">${Math.round(data.main.temp - 275.15)}°C</h2>
+    <h3 class="cloudtxt">${data.weather[0].description}</h3>`;
+}
+
+// Mostrar el contenido de la busqueda
+function rightSideContent(result) {
+  return `<div class="content">
+          <p class="title">NOMBRE</p>
+          <span class="value">${result.name}</span>
+        </div>
+        <div class="content">
+          <p class="title">TEMPERATURA</p>
+          <span class="value">${Math.round(result.main.temp - 275.15)}°C</span>
+        </div>
+        <div class="content">
+          <p class="title">HUMEDAD</p>
+          <span class="value">${result.main.humidity}%</span>
+        </div>
+        <div class="content">
+          <p class="title">VELOCIDAD VIENTO</p>
+          <span class="value">${result.wind.speed} Km/h</span>
+        </div>`;
+}
+
+async function displayForeCast(lat, long) {
+  const ForeCast_API = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${API}`;
+  const data = await fetch(ForeCast_API);
+  const result = await data.json();
+  // filtrar el pronostico
+  const uniqeForeCastDays = [];
+  const daysForecast = result.list.filter((forecast) => {
+    const forecastDate = new Date(forecast.dt_txt).getDate();
+    if (!uniqeForeCastDays.includes(forecastDate)) {
+      return uniqeForeCastDays.push(forecastDate);
+    }
+  });
+  console.log(daysForecast);
+
+  daysForecast.forEach((content, indx) => {
+    if (indx <= 3) {
+      listContentEl.insertAdjacentHTML("afterbegin", forecast(content));
+    }
+  });
+}
+
+// pronostico de datos en el html
+function forecast(frContent) {
+  const day = new Date(frContent.dt_txt);
+  const dayName = days[day.getDay()];
+  const splitDay = dayName.split("", 3);
+  const joinDay = splitDay.join("");
+
+  // console.log(dayName);
+
+  return `<li>
+  <img src="https://openweathermap.org/img/wn/${
+    frContent.weather[0].icon
+  }@2x.png" />
+  <span>${joinDay}</span>
+  <span class="day_temp">${Math.round(frContent.main.temp - 275.15)}°C</span>
+</li>`;
+}
